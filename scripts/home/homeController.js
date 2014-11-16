63b80988f920890006eacc2322ef7bb17f8d5efd
mchengal <mchengal@gmail.com>
2014-11-16 15:13:56 -0500

    stage0

--- scripts/home/homeController.js
+++ scripts/home/homeController.js
@@ -2,36 +2,21 @@
 
     'use strict';
 
-    angular.module('app').controller('home', home);
+    angular.module('app').controller('home',home);
 
     
-    function home(geoLocationService, dataService){
+    function home(){
 
         var vm =this;
-        vm.includeLocation = true;
-        vm.location = {};
+
         vm.message = "Hello from Home";
-        
 
         init();
         
         function init(){
-            
-            geoLocationService.getLocation().then(function(result){
-                vm.location = result;
-            });
-            
-            vm.message = dataService.getFirebaseRoot().toString();
-            
-            vm.foodTrucks = dataService.getData();
+            vm.message = "Hello from homeController init method";
         }
         
-        vm.save = function(){
-            
-            dataService.addData({name: vm.name|| '', text: vm.text || '', address: vm.location.address || ''}, 
-                                {latitude: vm.location.latitude, longitude: vm.location.longitude});
-            
-        }
     }
 })();
 
