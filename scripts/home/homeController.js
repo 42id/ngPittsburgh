3ee563ca5bb8e8e7fec3b57210214079365be365
bshir <brian.shirley@outlook.com>
2014-11-16 15:32:58 -0500

    stage 0

--- scripts/home/homeController.js
+++ scripts/home/homeController.js
@@ -2,31 +2,21 @@
 
     'use strict';
 
-    angular.module('app').controller('home', home);
+    angular.module('app').controller('home',home);
 
     
-    function home(geoLocationService){
+    function home(){
 
         var vm =this;
-        vm.includeLocation = true;
-        vm.location = {};
+
         vm.message = "Hello from Home";
-        
 
         init();
         
         function init(){
             vm.message = "Hello from homeController init method";
-            geoLocationService.getLocation().then(function(result){
-                vm.location = result;
-            });
         }
         
-        vm.save = function(){
-            
-            // Implement our save function to persist data to Firebase
-            
-        }
     }
 })();
 
