f2ba810360a9aa7e741d442d6700f50df7db0e9c
mchengal <mchengal@gmail.com>
2014-11-16 10:58:14 -0500

    reverting changes to stage0 r Please enter the commit message for your changes. Lines starting

--- scripts/home/homeController.js
+++ scripts/home/homeController.js
@@ -2,21 +2,36 @@
 
     'use strict';
 
-    angular.module('app').controller('home',home);
+    angular.module('app').controller('home', home);
 
     
-    function home(){
+    function home(geoLocationService, dataService){
 
         var vm =this;
-
+        vm.includeLocation = true;
+        vm.location = {};
         vm.message = "Hello from Home";
+        
 
         init();
         
         function init(){
-            vm.message = "Hello from homeController init method";
+            
+            geoLocationService.getLocation().then(function(result){
+                vm.location = result;
+            });
+            
+            vm.message = dataService.getFirebaseRoot().toString();
+            
+            vm.foodTrucks = dataService.getData();
         }
         
+        vm.save = function(){
+            
+            dataService.addData({name: vm.name|| '', text: vm.text || '', address: vm.location.address || ''}, 
+                                {latitude: vm.location.latitude, longitude: vm.location.longitude});
+            
+        }
     }
 })();
 
