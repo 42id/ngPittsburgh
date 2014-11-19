9671020c3fd028070b6a61ad3c6001185ff7c934
bshir <brian.shirley@outlook.com>
2014-11-18 19:14:27 -0500

    stage 1

--- scripts/home/homeController.js
+++ scripts/home/homeController.js
@@ -5,18 +5,32 @@
     angular.module('app').controller('home',home);
 
     
-    function home(){
+    function home(geoLocationService, dataService){
 
         var vm =this;
 
         vm.message = "Hello from Home";
+        vm.includeLocation = true;
+        vm.location = {};
 
         init();
         
         function init(){
             vm.message = "Hello from homeController init method";
+            geoLocationService.getLocation().then(function(result){
+                vm.location = result;
+            });
+            
+            vm.message = dataService.getFirebaseRoot().toString();
+            
+            vm.foodTrucks = dataService.getData();
         }
+        
+        vm.save = function(){
+    
+     dataService.addData({name: vm.name, text: vm.text, address: vm.location.address});
+    
+}
         
     }
 })();
-
